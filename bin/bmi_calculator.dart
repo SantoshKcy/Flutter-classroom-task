5e8f8c5b5650c71335cdd2f6f0342553347e87b3
SantoshKcy <kcsantosh2003@gmail.com>
2024-11-19 18:33:53 +0545

    changes

--- bin/bmi_calculator.dart
+++ bin/bmi_calculator.dart
@@ -13,7 +13,7 @@ void main() {
   String status;
   if (bmi < 18.5) {
     status = "Underweight";
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
+  } else if (bmi >= 18.5 && bmi <= 27.9) {
     status = "Normal weight";
   } else if (bmi >= 25 && bmi <= 29.9) {
     status = "Overweight";
